a74c6df2e7b5237739744125d0d9d500dca7cf58
Benearo Semidan Páez <benearosemidan@gmail.com>
2021-02-07 21:47:53 +0000

    Fixed interpolation miscalculation. Updated App Design project

--- FSRValue.m
+++ FSRValue.m
@@ -1,5 +1,5 @@
 function [vq] = FSRValue(xq)
-    x = [0 0.6 1.15 1.5 2.05 2.7]; % Weight values in Kg
-    v = [0 1.5 2 2.25 2.5 2.75]; % Voltage values in V
+    v = [0 0.1 0.5 1.0 1.1 1.2 1.3 1.4]; % Weight values in Kg
+    x = [0 2.2 2.57 3.67 4.02 4.06 4.08 4.09]; % Voltage values in V
     vq = interp1(x, v, xq, 'makima', 'extrap');
 end
